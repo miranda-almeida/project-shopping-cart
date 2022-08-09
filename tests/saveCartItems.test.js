05bb1f1a8e5b61363ee07e984927db691f9d5a2d
miranda <magui@duck.com>
2022-08-09 16:27:39 -0300

    requisito 6: testes função saveCartItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,13 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se localStorage.setItem é chamado ao executar saveCartItems', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  })
+  it('Verifica se localStorage.setItem é chamado ao executar saveCartItems com os parâmetros cartItems e o valor passado como argumento para saveCartItems', () => {
+    const ref = '<ol><li>Item</li></ol>';
+    saveCartItems(ref);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', ref);
+  })
 });
