11e5f5825b2f243d95e82eb24a2c8194fc35fb59
miranda <magui@duck.com>
2022-08-09 15:05:42 -0300

    requisito 1: testes fetchProducts

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,23 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  })
+  it('Verifica se fetch foi chamada com fetchProducts(computador)', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  })
+  it('Verifica se fetch utiliza o endpoint indicado', async () => {
+    const ENDPOINT = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toHaveBeenCalledWith(ENDPOINT);
+  })
+  it('Verifica se a estrutura de dados retornada do parâmetro computador está de acordo com computadorSearch', async () => {
+    const funcResponse = await fetchProducts('computador');
+    expect(funcResponse).toEqual(computadorSearch);
+  })
+  it('Verifica se um erro é retornado ao não receber argumento na função', async () => {
+    const funcResponse = await fetchProducts();
+    expect(funcResponse).toEqual(new Error('You must provide an url'));
+  })
 });
