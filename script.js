97adc3bb9535be9dc5ee142e8692da0dd34fb3da
miranda <magui@duck.com>
2022-08-10 12:49:49 -0300

    requisito 4: adiciona função createCart

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// captura e reproduz imagens de produtos
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -5,6 +6,7 @@ const createProductImageElement = (imageSource) => {
   return img;
 };
 
+// cria espaço no html para inserir os elementos dos produtos
 const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
@@ -12,6 +14,35 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
+// captura o identificador único de cada produto (sku)
+const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+
+// const cartItemClickListener = (event) => {
+
+// };
+
+// declarando a lista ordenada do carrinho de compras no escopo global para facilitar o uso em múltiplas funções
+const orderedList = document.querySelector('.cart__items');
+
+// configura as informações dos itens selecionados para o carrinho (com identificador, nome e preço)
+const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  // li.addEventListener('click', cartItemClickListener);
+  return li;
+};
+
+// função para filtrar a seleção do botão e direcionar para o carrinho, recebi ajuda da Isabela Costa para desenvolver a lógica
+const createCart = async (event) => {
+  const sku = getSkuFromProductItem(event.target.parentElement);
+  const results = await fetchItem(sku);
+  const { title, price } = results;
+  const productCart = { sku, title, price };
+  orderedList.appendChild(createCartItemElement(productCart));
+};
+
+// cria e anexa as informações do elemento do produto com sku, nome e imagem na listagem de produtos
 const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) => {
   const section = document.createElement('section');
   section.className = 'item';
@@ -19,11 +50,14 @@ const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) =>
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  button.addEventListener('click', createCart);
+  section.appendChild(button);
 
   return section;
 };
 
+// renderiza a listagem de produtos na página anexando os elementos organizados anteriormente
 const renderProducts = async () => {
   const { results } = await fetchProducts('computador');
   const section = document.querySelector('.items');
@@ -32,20 +66,6 @@ const renderProducts = async () => {
     section.appendChild(generateCards);
   });
 };
-
-// const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
-
-// const cartItemClickListener = (event) => {
-//   // coloque seu código aqui
-// };
-
-// const createCartItemElement = ({ sku, name, salePrice }) => {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// };
 
 window.onload = () => {
   renderProducts();
